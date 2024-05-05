e6f657a70955cff2d153b896c8e30a88a5e8d8c9
Nicole <114664530+Nicole212006@users.noreply.github.com>
2024-05-05 12:25:09 -0300

    oiii

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo = new Date("2024-12-29")
+const tempoObjetivo = new Date("2024-12-29T00;00;00");
+let tempoAtual = new Date();
 
-contadores[0].textContent = "tempoObjetivo";
+contadores[0].textContent = tempoObjetivo - tempoAtual;
